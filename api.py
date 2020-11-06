9e2fcadd57d5e38c0f7457b033fdaf6733082858
sp1d5r <aa1719@imperial.ac.uk>
2020-11-06 01:03:23 +0000

    trying to update conda

--- api.py
+++ api.py
@@ -63,7 +63,7 @@ def get_yesterday_information(country):
     return get_information_by_date(today.strftime("%Y-%m-%d"), country)
 
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='build', static_url_path='/')
 
 @app.route('/info_by_date/<country>/<date>')
 def info_by_date(country, date):
